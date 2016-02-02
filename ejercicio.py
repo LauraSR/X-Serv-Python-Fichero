f9e111cb584a17b6dd0f253214921cdac08287f7
laura sanz ruano <lsanz@zeta40.aulas.gsyc.es>
2016-02-02 13:00:07 +0100

    He realizado el ejercicio

--- ejercicio.py
+++ ejercicio.py
@@ -2,3 +2,14 @@
 # -*- coding: utf-8 -*-
 
 
+fich=open("/etc/passwd","r")
+
+while 1:
+    informacion = fich.readline()
+    usuario =  informacion.split(":") 
+    print usuario[0], usuario[-1][:-1]
+    if not informacion:
+        break
+
+fich.close()
+
